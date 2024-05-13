018820ce73868e64e60e7969847d78c15c2f85b6
Gabe1307 <123158394+Gabe1307@users.noreply.github.com>
2024-05-13 11:36:11 +0100

    bug fixes
    Bug Fixes

--- Assets/Scripts/HealthItem.cs
+++ Assets/Scripts/HealthItem.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class HealthItem : MonoBehaviour
 {
     public int healAmount = 20;  // Amount of health this item restores
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))  // Make sure to tag your player GameObject with "Player"
+        if (other.CompareTag("Player"))  
         {
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
                 playerHealth.Heal(healAmount);
-                Destroy(gameObject);  // Destroy the item after pickup
+                Destroy(gameObject);  
             }
         }
     }
